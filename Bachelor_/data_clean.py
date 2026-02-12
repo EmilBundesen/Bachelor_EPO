f8ef38d73ce35567ba365bd8931377f12c5fcba8
Emil Bundesen <din_github_email@gmail.com>
2026-02-12 20:53:45 +0100

    Opdatering med justering af daglig risikofri rente

--- Bachelor_/data_clean.py
+++ Bachelor_/data_clean.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 """
 Dette afsnit renser datasættet for det daglige afkast for 49 industrier
 """
-
 df = pd.read_csv(
     "/Users/emilbundesen/Desktop/Bachelor/Data/49_Industry_Portfolios_Daily.csv",
     sep=",",
@@ -14,22 +13,48 @@ df = pd.read_csv(
 )
 
 df = df[df[df.columns[0]].astype(str).str.match(r"^\d{8}$", na=False)] #Beholder run rækker med faktiske dage dvs. på formatet yyyymmdd
-
-df = df.rename(columns={df.columns[0]: "Date"})
-
+df = df.rename(columns={df.columns[0]: "Date"}) #navngivning
 df["Date"] = pd.to_datetime(df["Date"].astype(str), format="%Y%m%d") #Konverter til datatype date
-df = df.set_index("Date")
-
+df = df.set_index("Date") #indeksering
 for col in df.columns: #Konverter resten af kolonner til numerisk
     df[col] = pd.to_numeric(df[col], errors="coerce")
 
 df.replace([-99.99, -999], np.nan, inplace=True) #missing data
 
 start_date = "1985-01-01" #start for det data vi kigger på (afgrænsning)
-end_date = pd.Timestamp.today().strftime("%Y-%m-%d")  #i dag
+end_date = "2024-12-31" #her går data til både ved rf, så for at undgå metodiske komplikationer
 df = df.sort_index()
 df_clean = df.loc[start_date:end_date] #fjerner rækker udenfor tidsrækken
-df_clean = df_clean[~df_clean.index.duplicated(keep='first')]#Fjerner duplikerede rækker: 10300 rækker = 41 års data
+df_clean = df_clean[~df_clean.index.duplicated(keep='first')]#Fjerner duplikerede rækker: 10080 rækker = 40 års data
+
+"""
+Dette afsnit læser og korrigerer for rf, så vi kun analysere på merafkast. Vi bruger daglig rf
+"""
+
+rf_df = pd.read_csv(
+    "/Users/emilbundesen/Desktop/Bachelor/Data/F-F_Research_Data_Factors_daily.csv",
+    skiprows=3
+)
+
+rf_df = rf_df[rf_df.iloc[:,0].astype(str).str.match(r"^\d{8}$", na=False)] #Beholder run rækker med faktiske dage dvs. på formatet yyyymmdd
+rf_df = rf_df.rename(columns={rf_df.columns[0]: "Date"}) #navngiver 1 søjle
+rf_df["Date"] = pd.to_datetime(rf_df["Date"], format="%Y%m%d") #konverterer til datatype date
+rf_df = rf_df.set_index("Date") #indeksere på Date
+
+rf_df["RF"] = pd.to_numeric(rf_df["RF"], errors="coerce") / 100 #omskriver den risikofrie rente til numerisk værdi i pct.
+rf_df = rf_df.loc[start_date:end_date] #fjerner rækker udenfor tidsrækken
+rf_df = rf_df[~rf_df.index.duplicated(keep='first')] #Fjerner duplikerede rækker: 10080 rækker = 40 års data
+
+"""
+Dette afsnit sikrer, at rf og df_clean matcher
+"""
+
+df_clean, rf = df_clean.align(rf_df["RF"], join="inner", axis=0)
+
+# Sanity check
+print("Dataframe shape:", df_clean.shape)
+print("RF shape:", rf.shape)
+print("Periode:", df_clean.index.min(), "til", df_clean.index.max())
 
 """
 Denne sektion finder og viser bedste og værste industrier
@@ -37,32 +62,37 @@ Denne sektion finder og viser bedste og værste industrier
 Der skal korrigeres med den risikofrierente, så vi kun kigger på merafkast
 """
 
+daglig_afkast = df_clean.fillna(0) / 100 #man kunne ændre måden vi håndtere manglende data
+
+kum = (1 + daglig_afkast).cumprod()
+kum_rf = (1 + rf).cumprod()
+
+kum_merafkast = kum.div(kum_rf, axis=0) - 1 #Ratio-metode
 
 
-daglig_afkast = df_clean / 100  #Da værdierne er i %
-
-kum_afkast = (1 + daglig_afkast).cumprod() - 1
-
-sidste_dag_afkast = kum_afkast.iloc[-1]  # kumulativt afkast på sidste dag
+#Bedste og dårligste industrier
+sidste_dag_afkast = kum_merafkast.iloc[-1]
 top3 = sidste_dag_afkast.sort_values(ascending=False).head(3).index
 bottom3 = sidste_dag_afkast.sort_values(ascending=True).head(3).index
 
 print(f"Top 3 industrier: {top3}")
 print(f"Dårligste 3 industrier: {bottom3}")
 
+"""
+Denne sektion plotter kumulativt afkast
+"""
+
 plt.figure(figsize=(14,7))
 
-# Top 3
 for industry in top3:
-    plt.plot(kum_afkast.index, kum_afkast[industry], label=f"Top: {industry}", linewidth=2)
+    plt.plot(kum_merafkast.index, kum_merafkast[industry], label=f"Top: {industry}", linewidth=2)
 
-# Bottom 3
 for industry in bottom3:
-    plt.plot(kum_afkast.index, kum_afkast[industry], label=f"Bottom: {industry}", linestyle='--', linewidth=2)
+    plt.plot(kum_merafkast.index, kum_merafkast[industry], label=f"Bottom: {industry}", linestyle='--', linewidth=2)
 
-plt.title("Kumulativt dagligt afkast (1985–2025) – Top 3 vs. Bottom 3 industrier")
+plt.title("Kumulativt dagligt merafkast (1985–2024) – Top 3 vs. Bottom 3 industrier")
 plt.xlabel("Date")
-plt.ylabel("Kumulativt afkast")
+plt.ylabel("Kumulativt merafkast")
 plt.grid(True)
 plt.legend()
 plt.show()
